14656902ab3f18fad2e93bc12f4674dbfb93a995
NojusIlekis <b00100794@mytudublin.ie>
2025-04-22 12:24:53 +0100

    fixed not found checkout error

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;      // ← added
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;      // ← added
 
 /*
 |--------------------------------------------------------------------------
